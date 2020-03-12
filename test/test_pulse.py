35559baf903f21985c8dda1276fcfa978d5ab708
nasihs <nasihs1113@gmail.com>
2020-03-12 17:16:56 +0800

    test pulse

---
+++ test/test_pulse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: uft-8 -*-
+"""测试pulse
+"""
+
+
+# from time import sleep
+import Adafruit_PCA9685
+
+
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(60)
+
+pulse = int(input('input pulse:'))
+pwm.set_pwm(0, 0, pulse)
