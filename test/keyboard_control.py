21103b0a459e1dd1eaede31f69481d9b6df05542
nasihs <nasihs1113@gmail.com>
2020-03-12 20:44:33 +0800

    updated

--- test/keyboard_control.py
+++ test/keyboard_control.py
@@ -1,47 +1,68 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-import time
+
+from time import sleep
 
 import Adafruit_PCA9685
 from pynput import keyboard
 
 
-class Motor(object):
-    def __init__(self, channel, freq):
-        self._pwm = Adafruit_PCA9685.PCA9685()
-        self._pwm.set_pwm_freq(freq)
-        self._channel = channel
+class Vehicle(object):
 
-    def run(self, pulse):
-        self._pwm.set_pwm(self._channel, 0, pulse)
+    def __init__(self, channel_motor=0, channel_servo=3, freq=60):
+        self._pwm = Adafruit_PCA9685()
+        self._channel_motor = channel_motor
+        self._channel_servo = channel_servo
+        self._pwm.set_pwm_freq(freq)
+
+    def unlock_esc(self):
+        self._pwm.set_pwm(self._channel_motor, 0, 307)  # 电调解锁方式尚不明确
+        sleep(2)
+        # print('unlock')
+        return True
+
+    def run(self, pulse=407):
+        print('run')
+        pass
 
     def stop(self):
-        self._pwm.set_pwm(self._channel, 0, 185)
+        print('stop')
+        # self._pwm.set_pwm(self._channel, 0, 185)
+        pass
+
+    def turn(self, pulse=300):
+        print('turn')
+        pass
 
 
-class Servo(object):
-    def __init__(self, channel, freq):
-        self._pwm = Adafruit_PCA9685.PCA9685()
-        self._pwm.set_pwm_freq(freq)
-        self._channel = channel
-    
-    def turn(self, direction):
-        if direction == 'left':
-            print('Left')
-            pass
-        else:
-            print('Right')
-            pass
-    
+car1 = Vehicle()
+current_key = None
 
 
 def on_press(key):
-    if key.char == 'i':
-        print()
+    global current_key
+    if key.char != current_key:
+        current_key = key.char
+        if key.char == 'w':
+            car1.run()
+        elif key.char == 's':
+            car1.run()
+        elif key.char == 'a':
+            car1.turn()
+        elif key.char == 'd':
+            car1.turn()
+        elif key.char == 'q':  # 电调解锁
+            car1.unlock_esc()
+    elif key.char == current_key:
+        pass
 
 
 def on_release(key):
-    print('key_up')
+    if key.char == 'w':
+        car1.stop()
+    elif key.char == 's':
+        car1.stop()
 
 
 # 监听键盘按键
