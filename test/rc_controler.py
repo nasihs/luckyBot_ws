76c230e3682117519b60230994e5c0762a673e59
nasihs <nasihs1113@gmail.com>
2020-03-15 15:20:09 +0800

    目标ip作为命令行参数传入

--- test/rc_controler.py
+++ test/rc_controler.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""RC遥控车控制端
+目标ip作为命令行参数传入 默认为 '192.168.50.88'
+"""
+
 
 import sys
 import socket
 from pynput import keyboard
 
 
-# HOST = '192.168.50.88'
+HOST = '192.168.50.88'
 PORT = 1201
 BUFSIZE = 1024
 # ADDR = (HOST, PORT)
@@ -35,11 +39,8 @@ def on_release(key):
     sock.send(b'stop')
 
 
-if not sys.argv == sys.argv[0]:
-    host = '{0}'.format(sys.argv[1])
-else:
-    host = '192.168.50.88'
-print('target server{0} : {1}'.format(host, PORT))
+host = '{0}'.format(sys.argv[1]) if len(sys.argv) > 1 else HOST
+print('target server{0}:{1}'.format(host, PORT))
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, PORT))
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
