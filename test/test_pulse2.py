024bfeae0854ac69fa1025dfcb92d74702a51a86
nasihs <nasihs1113@gmail.com>
2020-03-16 14:03:36 +0800

    test

--- test/test_pulse2.py
+++ test/test_pulse2.py
@@ -6,12 +6,17 @@
 
 
 # from time import sleep
+# import sys
+
 import Adafruit_PCA9685
 
 
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(60)
 
+channel = int(input('input channel'))
 pulse = int(input('input pulse:'))
-while True: 
-    pwm.set_pwm(0, 0, pulse)
+print('channel:{0} pulse:{1}'.format(channel, pulse))
+# channel, pulse = '{0}'.format(sys.argv[1]) if len(sys.argv) > 1 else 0, 375
+
+pwm.set_pwm(channel, 0, pulse)
