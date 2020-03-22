79f1b9ec1127c16f71cb5afa3e70ca5aaf9f320e
nasihs <nasihs1113@gmail.com>
2020-03-22 19:42:01 +0800

    test

---
+++ test_py2/local.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+"""本地图片编码解码测试
+"""
+
+
+from __future__ import print_function
+# import sys
+# import socket
+# import struct
+
+import cv2
+import numpy as np
+
+
+frame = cv2.imread('./3.jpg')
+frame_array = np.array(frame)
+frame_bytes = frame_array.tostring()
+data = np.frombuffer(frame_bytes)
+frame1 = cv2.decode(data, 1)
+cv2.imshow('cam', frame1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
